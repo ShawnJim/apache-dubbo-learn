c34f52a5b171659d11da6bec9983297845c076cf
jzx <2510365715@qq.com>
2020-06-13 17:11:54 +0800

    初始化项目

---
+++ src/main/java/com/shawn/apache/dubbo/ApacheDubboLearnApplication.java
@@ -0,0 +1,13 @@
+package com.shawn.apache.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheDubboLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheDubboLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shawn/apache/dubbo/ApacheDubboLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawn.apache.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheDubboLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
