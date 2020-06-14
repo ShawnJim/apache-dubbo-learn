b1c57938a429d5bd41755550fa200fc20e0b0fce
jzx <2510365715@qq.com>
2020-06-14 15:45:30 +0800

    dubbo序列化策略 & 接入nacos外部化配置

--- apache-dubbo-consumer/src/main/java/com/shawn/apache/dubbo/consumer/controller/EchoController.java
+++ apache-dubbo-consumer/src/main/java/com/shawn/apache/dubbo/consumer/controller/EchoController.java
@@ -2,18 +2,24 @@ package com.shawn.apache.dubbo.consumer.controller;
 
 import com.shawn.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class EchoController {
 
     @Reference(version = "1.0.0")
     EchoService echoService;
 
+    @Value("${user.name}")
+    private String userName;
+
     @GetMapping("/echo/{string}")
     public String echo(@PathVariable String string){
-        return echoService.echo(string);
+        return echoService.echo(string)+" "+userName;
     }
 }
--- apache-dubbo-provider/apache-dubbo-provider-service/src/main/java/com/shawn/apache/dubbo/provider/service/EchoServiceImpl.java
+++ apache-dubbo-provider/apache-dubbo-provider-service/src/main/java/com/shawn/apache/dubbo/provider/service/EchoServiceImpl.java
@@ -2,11 +2,16 @@ package com.shawn.apache.dubbo.provider.service;
 
 import com.shawn.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service(version = "1.0.0")
 public class EchoServiceImpl implements EchoService {
+
+    @Value("${dubbo.protocol.port}")
+    private String port;
+
     @Override
     public String echo(String string) {
-        return "Dubbo service echo ："+string;
+        return "Dubbo service echo ："+string+" port :"+port;
     }
 }
